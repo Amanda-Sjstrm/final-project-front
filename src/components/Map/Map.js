124cf9fd1f181a2992191870e59a6758a3bf95ea
Amanda <amanda.sl747@gmail.com>
2023-06-15 14:31:23 +0200

    Update Map.js

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -10,7 +10,7 @@ export const Map = ({ setCoordinates, setBounds, coordinates, places, setChildCl
   return (
     <MapContainer>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.GOOGE_API }}
+        bootstrapURLKeys={{ key: process.env.GOOGLE_API }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
