ce9860fcf2101ce9bac38735352a490834dad7a4
Amanda <amanda.sl747@gmail.com>
2023-06-19 13:55:24 +0200

    Update StyledInfo.js

--- src/components/Info/StyledInfo.js
+++ src/components/Info/StyledInfo.js
@@ -14,4 +14,9 @@ export const StyledH2 = styled.h2`
 export const StyledP = styled.p`
   font-size: 18px;
   margin: 20px 50px;
+
+  @media (max-width: 668px) {
+    margin: 0;
+    text-align: center;
+  }
 `;
