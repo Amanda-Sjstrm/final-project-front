ed756be668d27428a5726193c31a564307819225
Amanda <amanda.sl747@gmail.com>
2023-06-19 13:57:20 +0200

    Update StyledMain.js

--- src/components/Main/StyledMain.js
+++ src/components/Main/StyledMain.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const MapWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 90%;
+  margin: 0 auto;
 
-  @media (min-width: 668px) {
+  @media (max-width: 668px) {
     display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    width: 90%;
-    margin: 0 auto;
+    flex-direction: column;
+    justify-content: center;
   }
 `;
